1a7017ca40fec10f4b75f27ef72aa2746ae66ede
github username <github email address>
2020-06-20 13:15:43 -0700

    stuck trying to convert int to str

--- c-program.c
+++ c-program.c
@@ -4,11 +4,16 @@ int main() {
     char word[] = "hang";
     int number = 5;
     char guess;
-    printf("Enter a letter: \n");
-    printf("<You have %d guesses remaining>\n", number);
-    scanf("%c", &guess); 
-    number -= 1;     
-    
+    scanf(fun(itoa(number, 10), guess), &guess);
+
     printf("Your guess is %c\n", guess);
     return 0;
-}
+};
+
+char fun(number, guess){
+    char str;
+    printf("Enter a letter: \n");
+    printf("<You have %d guesses remaining>\n", number);
+    scanf("%c", &str);
+    return str;    
+};
