6aef9113e7f7cf2f26a44d7b1109f784f793dfd4
github username <github email address>
2020-06-24 12:34:38 -0700

    havinf trouble keeping track of all corectly guessed letters

--- c-program.c
+++ c-program.c
@@ -4,16 +4,26 @@ int main() {
     char word[] = "hang";
     int number = 5;
     char guess;
+    char arr[] = "h";
+    int len = 0;
     while(number > 0){
         printf("Enter a letter: \n");
         scanf(" %c", &guess);
-        if (!strchr(word, guess)){
+        char *p = strchr(word, guess);
+        if (!p){
             printf("Your guess %c is incorrect\n", guess);
+            number -= 1;
         } else {
+            int q = p - word;
+            arr[len] = q;
+            len += 1;
             printf("Your guess is correct\n");
-        }
-        number -= 1;
-        printf("<You have %d guesses remaining>\n", number);
+            // printf("%c\n", word[q]);
+        };
+        for (int i=0; word[i] != '\0'; i++){
+            printf("%s", arr);
+        };
+        printf("<You have %d strikes remaining>\n", number);
     };
     return 0;
 };
